5acd78c87d0ce5f9e07d04bc435b060f5cc5dae4
wmk567 <13520020@std.stei.itb.ac.id>
2022-11-22 18:12:31 +0700

    hapus file yang tidak dibutuhkan

--- src/App.tsx
+++ src/App.tsx
@@ -4,7 +4,6 @@ import Singer_content from './components/singer-content';
 import Register from './components/register'
 import Login from './components/login'
 import Subscription from './components/subscription';
-import Add_song from './components/add-song';
 import Premium_song from './components/song';
 import { BrowserRouter as Router, Routes, Route, Link } from 'react-router-dom'
 import './css/global.css'
@@ -13,7 +12,6 @@ import './css/list-display.css'
 import './css/login.css'
 import './css/management.css'
 import './css/singer-content.css'
-import './css/add-song.css'
 import './css/song.css'
 {/*import Premium_singer from './components/premium-singer';*/}
 {/*import Premium_singer_list from './components/premium-singer-list';*/}
@@ -27,7 +25,6 @@ const App:React.FC = () => {
           <Route path = "/" element = {<><Login /></>} />
           <Route path = "/your-songs" element = {<><Navbar /><Singer_content /></>} />
           <Route path = "/subscription" element = {<><Navbar /><Subscription /></>} />
-          <Route path = "/add-song" element = {<><Navbar /><Add_song /></>} />
           <Route path = "/song" element = {<><Navbar /><Premium_song /></>} />
           <Route path = "/register" element = {<><Register /></>} />
         </Routes>
